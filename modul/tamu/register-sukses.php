22fa50ea692b6f420a8c61d94f884bb9ed3235f8
fahm <fahm@localhost.localdomain>
2014-12-18 01:33:22 +0700

    upload sukses (no validation)

---
+++ modul/tamu/register-sukses.php
@@ -0,0 +1,29 @@
+
+<div class="section section-breadcrumbs">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h1>Register User</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+        
+        <div class="section">
+	    	<div class="container">
+				<div class="row">
+                                    
+					<div class="col-sm-12">
+						<div class="error-page-wrapper">
+							<p>Akun anda berhasil dibuat, silahkan <a href="index.php?module=login">Login <i class="fa fa-sign-in"></i></a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
